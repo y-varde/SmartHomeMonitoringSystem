7b5ba6d50c92b04b6917e9cb9f25715fbd799b91
yashv3 <yashv3@uw.edu>
2024-11-26 18:55:14 -0800

    Integrated an lcd display into central device via hardware I2C for displaying sensor readings and monitoring communication changes

--- Central/Central.ino
+++ Central/Central.ino
@@ -1,20 +1,30 @@
 #include "DHT11.h"
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include "LiquidCrystal_I2C.h"
 
+// Initialize the DHT11 sensor
 DHT11 dht11(2);
-const int gasPin = A5;
+const int gasPin = A3;
 const int tempPin = A1;
 const int buzzerPin = 8; // Define the buzzer pin
 
+// Initialize Bluetooth serial communication
 SoftwareSerial btSerial(4, 3); // RX, TX
+
+// Create an LCD object with I2C address 0x27, 20 columns, and 4 rows
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 bool isArmed = false;
 int samplingRate = 1000; // Default sampling rate in milliseconds
 
-void setup() 
-{
+void setup() {
   Serial.begin(9600);
   btSerial.begin(9600); // Initialize Bluetooth serial communication
+
+  Wire.begin();    // Initialize I2C communication
+  lcd.init();      // Initialize the LCD
+  lcd.backlight(); // Turn on the backlight
 }
 
 void loop() {
@@ -53,11 +63,15 @@ void checkBluetoothCommand() {
 void armSystem() {
   isArmed = true;
   Serial.println("System is armed");
+  lcd.setCursor(0, 3);
+  lcd.print("System is armed");
 }
 
 void disarmSystem() {
   isArmed = false;
   Serial.println("System is disarmed");
+  lcd.setCursor(0, 3);
+  lcd.print("System is disarmed");
 }
 
 void setSamplingRate() {
@@ -76,8 +90,14 @@ void setSamplingRate() {
     Serial.print("Sampling rate set to ");
     Serial.print(samplingRate / 1000);
     Serial.println(" seconds");
+    lcd.setCursor(0, 3);
+    lcd.print("Sampling rate: ");
+    lcd.print(samplingRate / 1000);
+    lcd.print(" sec");
   } else {
     Serial.println("Invalid sampling rate");
+    lcd.setCursor(0, 3);
+    lcd.print("Invalid sampling rate");
   }
 }
 
@@ -93,6 +113,12 @@ String readTemperature() {
   } else {
     stopBuzzer();
   }
+
+  Serial.print(tempData);
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print((int)temp);
+  lcd.print(" C");
 
   return tempData;
 }
@@ -110,6 +136,12 @@ String readGasConcentration() {
     stopBuzzer();
   }
 
+  Serial.print(gasData);
+  lcd.setCursor(0, 1);
+  lcd.print("Gas: ");
+  lcd.print((int)gas);
+  lcd.print(" ppm");
+
   return gasData;
 }
 
@@ -118,6 +150,12 @@ String readHumidity() {
   float humidity = getHumidity();
   humidityData += String((int)humidity);
   humidityData += "%\n";
+
+  Serial.print(humidityData);
+  lcd.setCursor(0, 2);
+  lcd.print("Humidity: ");
+  lcd.print((int)humidity);
+  lcd.print("%");
 
   return humidityData;
 }
